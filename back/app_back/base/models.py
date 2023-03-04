b1670e716d927b61e89251f4d19e7af3a4542d9d
cerealwhay <cerealwhay@gmail.com>
2023-03-04 20:24:06 +0300

    - сделал отправку массива на беке с сортировкой по убыванию рейтинга - сделал проверку на юзернейм, и не создавать такой же если он уже есть. Но при условии, что результат у этого бзернейма выше чем уже был до этого, то заменять его результат

--- back/app_back/base/models.py
+++ back/app_back/base/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class ScoreRecordItem(models.Model):
     username = models.CharField(max_length=100)
     score = models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now=True)
